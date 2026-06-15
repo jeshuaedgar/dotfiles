aabba55a45fa132f423e1a8890bc8eca2a3d0b5a
jeshuaedgar <67895779+jeshuaedgar@users.noreply.github.com>
2026-06-15 17:05:56 +0200

    remove: comments

--- dot_config/nvim/lua/plugins/coloscheme.lua
+++ dot_config/nvim/lua/plugins/coloscheme.lua
@@ -1,8 +1,5 @@
 return {
-  -- add gruvbox
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
